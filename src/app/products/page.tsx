836626ae4d9b691a2df0426900afca0de8f7ea2a
zoritto <zoritto@zoritto.zoritto>
2025-02-21 07:54:33 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">products page</h1>;<Button>FSW .aaa</Button>
+      <Input placeholder="bora terminar" />
+    </div>
+  );
+};
+
+export default ProductPage;
